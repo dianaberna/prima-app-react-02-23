af7e0e04a965a1134d871e67c102b1d6002711b3
Diana Bernabei <bernabei.d@gmail.com>
2023-03-28 11:30:09 +0200

    aggiunti commenti

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,5 +1,10 @@
 import Navbar from "../Navbar";
 import React from "react";
+// se non inserisco index.js all'interno della cartella Header devo fare una import come questa qui sotto
+// import Header from "../Header/Header"
+// .. -> ci fa risalire nella cartella genitore
+// se nella cartella Header inserisco il file index.js dove al suo interno ho messo a disposizione il componente
+// Header allora posso semplicemente mettere il nome della cartella e "react" in automatico entra nel file index
 import Header from "../Header";
 
 function Layout(){
